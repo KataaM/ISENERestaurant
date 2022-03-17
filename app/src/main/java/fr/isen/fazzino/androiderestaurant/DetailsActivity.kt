e12ff64c8c3d09ecb9c4ff75d227b05c33708901
KataM <gregofazz@hotmail.fr>
2022-03-17 16:35:34 +0100

    Ajout des boutons pour selectionner la quantité et afficher le prix total

--- app/src/main/java/fr/isen/fazzino/androiderestaurant/DetailsActivity.kt
+++ app/src/main/java/fr/isen/fazzino/androiderestaurant/DetailsActivity.kt
@@ -41,9 +41,40 @@ class DetailsActivity : AppCompatActivity() {
 
 
         binding.imageSlider.adapter = DetailImagePager(this,dataDish.images)
+
+        binding.priceButton.text = dataDish.prices[0].price.toString() + " €"
 //        binding.detailsParagraph.text = dishName?.name_fr
 //        val dishName: Category? =
 //            dataDish?.data?.find { categoryData -> categoryData.name_fr == categoryName }
 
+
+        binding.quantityTextView.text = "1"
+
+        setUpListener()
+    }
+
+    fun setUpListener(){
+        var count = 1.0f
+
+        binding.removeButton.setOnClickListener {
+            if (count > 1){
+                count--
+            }
+            refreshTextView(count)
+        }
+
+        binding.addButton.setOnClickListener {
+            count++
+            refreshTextView(count)
+        }
+    }
+
+    fun refreshTextView(count : Float){
+        val dataDish =
+            intent.getSerializableExtra(CategoryActivity.DATA_DISH_OBJECT_KEY) as Dish
+
+        val price = count * dataDish.prices[0].price
+        binding.quantityTextView.text = count.toString()
+        binding.priceButton.text = price.toString() + " €"
     }
 }
